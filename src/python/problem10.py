169e18ee126b3c42ca7e6a627bc3b14939f25240
Sachin Lohith <sachinlohith93@gmail.com>
2016-12-21 00:25:17 -0800

    Added Sieve of Eratosthenes

---
+++ src/python/problem10.py
@@ -0,0 +1,19 @@
+'''
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+'''
+
+from util import genPrimes
+
+
+def solution():
+    primes = [2]
+    prime = genPrimes()
+    while primes[-1] < 2000000:
+        primes.append(next(prime))
+    return sum(primes[1:-1])
+
+
+if __name__ == "__main__":
+    print solution()
--- src/python/util.py
+++ src/python/util.py
@@ -9,3 +9,21 @@ def divisibleByWholeList(number, factors):
 
 def divisibleByAnyInList(number, factors):
     return any(map(lambda x: number % x == 0, factors))
+
+
+def genPrimes():
+    '''
+    Generate an infinte sequence of primes
+    '''
+
+    witnessMap = {}
+    composite = 2
+    while True:
+        if composite not in witnessMap:
+            yield composite
+            witnessMap[composite*composite] = [composite]
+        else:
+            for prime in witnessMap[composite]:
+                witnessMap.setdefault(prime + composite, []).append(prime)
+            del witnessMap[composite]
+        composite += 1
